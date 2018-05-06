2dfea2e4a6c7e9cf4e72e17112bc969236df3d00
simpleap <gerlis1997@gmail.com>
2018-05-06 15:35:08 -0500

    Initial Commit

---
+++ Platzzel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Platzzel
+//
+//  Created by SimpleAp on 6/05/18.
+//  Copyright © 2018 SimpleAp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
